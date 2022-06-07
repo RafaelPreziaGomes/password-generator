55b518515a29d4bc6b5ded382deddd5c165fb511
RafaelPreziaGomes <rafafprezia@gmail.com>
2022-06-07 16:53:22 -0400

    fized initial bugs and errors

--- script.js
+++ script.js
@@ -180,8 +180,6 @@ document.querySelector("#generate").addEventListener("click", function () {
     finalArray.push(item);
   }
 
-  // transform final array into a string
-
   var password = finalArray.join("");
 
   console.log(password);
